c264e313f05ad471689fe8fadb5d28326e717b1c
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2016-11-09 22:10:25 -0500

    updated storecontroller

--- COMP229-F2016-Lesson9/Controllers/StoreController.cs
+++ COMP229-F2016-Lesson9/Controllers/StoreController.cs
@@ -17,15 +17,16 @@ namespace COMP229_F2016_Lesson9.Controllers
         // GET: Store
         public ActionResult Index()
         {
+            /*
             List<Genre> genres = new List<Genre>
             {
                 new Genre("Disco"),
                 new Genre("Jazz"),
                 new Genre("Rock"),
                 new Genre("Electronic")
-            };
+            }; */
 
-            return View(genres);
+            return View(db.Genres.ToList());
         }
 
         // GET: Store/Browse?genre=Disco
